63f8618bb008c6ca1d97b04d10be0bedff829006
Khoa <Khoa@DESKTOP-BO320VM>
2021-12-05 14:05:47 +0700

    update

--- src/main/java/api_testing/LaunchApp.java
+++ src/main/java/api_testing/LaunchApp.java
@@ -1,20 +1,32 @@
 package api_testing;
 
 import caps.MobileCapabilityTypeEx;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
 
 public class LaunchApp {
     public static void main(String[] args) {
+        try {
+            AppiumDriver<MobileElement> appiumDriver = null;
 
-        //Specify capacities
-
+            //Specify capacities
         DesiredCapabilities desiredCaps = new DesiredCapabilities();
         desiredCaps.setCapability(MobileCapabilityTypeEx.PLATFORM_NAME, "Android");
         desiredCaps.setCapability(MobileCapabilityTypeEx.AUTOMATION_NAME, "uiautomator2");
-        desiredCaps.setCapability(MobileCapabilityTypeEx.UDID, "");
-        desiredCaps.setCapability(MobileCapabilityTypeEx.APP_PACKAGE, "");
-        desiredCaps.setCapability(MobileCapabilityTypeEx.APP_ACTIVITY, "");
+        desiredCaps.setCapability(MobileCapabilityTypeEx.UDID, "ce11171beb27330301");
+        desiredCaps.setCapability(MobileCapabilityTypeEx.APP_PACKAGE, "com.wdiodemoapp");
+        desiredCaps.setCapability(MobileCapabilityTypeEx.APP_ACTIVITY, "com.wdiodemoapp.MainActivity");
 
+            //Send the desiredCaps into appium server
+            URL appiumServer = new URL("http://127.0.0.1:4723");
+            appiumDriver = new AndroidDriver<MobileElement>(appiumServer,desiredCaps);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         //init and create an appium session client <---> appium server
 
 
